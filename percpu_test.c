e6afa344e8d6e7f2d83ea90f6f63047395350c81
kwpark <kunwook9826@gmail.com>
2023-07-06 07:37:35 +0000

    Linter

--- percpu_test.c
+++ percpu_test.c
@@ -5,7 +5,6 @@
 #include <linux/percpu-defs.h>
 #include <linux/sched.h>
 
-
 /* DESCRIPTION
 
 1) allocate
@@ -35,7 +34,7 @@
 */
 
 int __percpu *my_percpu_dynamic;
-DEFINE_PER_CPU(int, my_percpu_macro);			//allocate percpu<1>
+DEFINE_PER_CPU(int, my_percpu_macro); //allocate percpu<1>
 
 int i;
 int cpu;
@@ -43,34 +42,35 @@ int *ptr;
 
 static int __init start_module(void)
 {
-  printk(KERN_INFO "percpu_test: module loaded\n");
+	printk(KERN_INFO "percpu_test: module loaded\n");
 
-	my_percpu_dynamic = alloc_percpu(int);	//allocate percpu<2>
+	my_percpu_dynamic = alloc_percpu(int); //allocate percpu<2>
 
-	cpu = get_cpu();  //preemption enable
-	per_cpu(my_percpu_macro, cpu) = 10000000;	
+	cpu = get_cpu(); //preemption enable
+	per_cpu(my_percpu_macro, cpu) = 10000000;
 	per_cpu(*my_percpu_dynamic, cpu) = 10000000;
 	(*per_cpu_ptr(my_percpu_dynamic, cpu)) *= 2;
-	put_cpu();				//preemption disable
+	put_cpu(); //preemption disable
 
-	for(i=0; i<1000000; i++) {
-		ptr = get_cpu_ptr(my_percpu_dynamic);	//preemption enable
+	for (i = 0; i < 1000000; i++) {
+		ptr = get_cpu_ptr(my_percpu_dynamic); //preemption enable
 		*ptr += 1;
-		put_cpu_ptr(my_percpu_dynamic);				//preemption disable
+		put_cpu_ptr(my_percpu_dynamic); //preemption disable
 	}
-  return 0;
+	return 0;
 }
 
 static void __exit end_module(void)
 {
 	for_each_possible_cpu(cpu) {
-		printk(KERN_INFO "percpu_test: cpu[%d] => %d(macro), %d(dynamic)", cpu, per_cpu(my_percpu_macro, cpu), *per_cpu_ptr(my_percpu_dynamic, cpu));
+		printk(KERN_INFO
+		       "percpu_test: cpu[%d] => %d(macro), %d(dynamic)",
+		       cpu, per_cpu(my_percpu_macro, cpu),
+		       *per_cpu_ptr(my_percpu_dynamic, cpu));
 	}
 	free_percpu(my_percpu_dynamic);
 	printk(KERN_INFO "percpu_test: module unloaded\n");
 }
-
-
 
 ///< The author -- visible when you use modinfo
 MODULE_AUTHOR("Kunwook Park");
